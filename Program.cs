510c9a3da80e9b56f8d4aec71d485986581a8acb
Contz Alexandru <151956417+contz-alexandru@users.noreply.github.com>
2024-04-10 12:49:30 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,69 +1,52 @@
 // Complicated Calculator V1 by Contz Alexandru
 // No Regex Usage
+
 namespace ConsoleApp1
 {
     internal class Program
     {
         const string operators = @"+-*/:^%";
-        public double Operation(double x, double y, char o)
-        {
-            switch (o)
-            {
-                case '+':
-                    return x + y;
-                case '-':
-                    return x - y;
-                case '*':
-                    return x * y;
-                case '/' or ':':
-                    return x / y;
-                case '%':
-                    return x % y;
-                case '^':
-
-                    break;
-            }
-        }
+        const string points = @".,";
         static void Main(string[] args)
         {
-            // Read user input
-            Console.Write("Enter your math problem (+,-,*,/,:,^,%): ");
-            string input = Console.ReadLine();
-            while (string.IsNullOrEmpty(input))
+            string input = string.Empty;
+            bool hasDigit = false;
+
+            // Checking for digits and filtering imput
+            while (hasDigit is false)
             {
-                Console.WriteLine("Your input is empty, try again!");
                 Console.Write("Enter your math problem (+,-,*,/,:,^,%): ");
                 input = Console.ReadLine();
+                while (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Your input is empty, try again!");
+                    Console.Write("Enter your math problem (+,-,*,/,:,^,%): ");
+                    input = Console.ReadLine();
+                }
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsDigit(input[i]) is false && operators.Contains(input[i]) is false && ",.".Contains(input[i]) is false)
+                    {
+                        input = input.Remove(i, 1);
+                        i--;
+                    }
+                    else if (char.IsDigit(input[i]))
+                        hasDigit = true;
+                }
+                if (!hasDigit)
+                {
+                    Console.WriteLine("No supported math problem available!, Try again!");
+                }
+
             }
-            // Actual Program
-            double[] numberArray = new double[input.Length - 1];
-            string[] operatorArray = new string[input.Length - 1];
-            int arrayIndex = 0;
-            string numberString = string.Empty;
+            Console.WriteLine(input);
+
+            // Filtering symbols
+
             for (int i = 0; i < input.Length; i++)
             {
-                if (char.IsDigit(input[i]))
-                {
-                    numberString += input[i];
-                }
-                else if (",.".Contains(input[i]) && numberString != string.Empty)
-                {
-                    if (numberString.EndsWith(',') is false && numberString.EndsWith('.') is false)
-                    {
-                        numberString += input[i];
-                    }
-                }
-                else if (operators.Contains(input[i]))
-                {
-                    if () { }
-                }
-                else
-                {
-                    input = input.Remove(i, 1);
-                    i--;
-                }
+                break;
             }
-
         }
     }
 }
