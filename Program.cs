5ef589f67407c0fbe56160c07936df868ad3ea0b
Contz Alexandru <151956417+contz-alexandru@users.noreply.github.com>
2024-04-15 09:18:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
             string input = string.Empty;
             bool hasDigit = false;
 
-            // Checking for digits and filtering imput
+            // Checking for digits and filtering input
             while (hasDigit is false)
             {
                 Console.Write("Enter your math problem (+,-,*,/,:,^,%): ");
@@ -25,7 +25,7 @@ namespace ConsoleApp1
                 }
                 for (int i = 0; i < input.Length; i++)
                 {
-                    if (char.IsDigit(input[i]) is false && operators.Contains(input[i]) is false && ",.".Contains(input[i]) is false)
+                    if (char.IsDigit(input[i]) is false && operators.Contains(input[i]) is false && points.Contains(input[i]) is false)
                     {
                         input = input.Remove(i, 1);
                         i--;
@@ -37,15 +37,50 @@ namespace ConsoleApp1
                 {
                     Console.WriteLine("No supported math problem available!, Try again!");
                 }
-
             }
             Console.WriteLine(input);
 
-            // Filtering symbols
+            //
+            string[] operatorArray = new string[input.Length];
 
+            string numberString = string.Empty;
+            double[] numberArray = new double[input.Length];
+
+            int j = 0;
+            // Filter operators
             for (int i = 0; i < input.Length; i++)
             {
-                break;
+                if (operators.Contains(input[i]))
+                {
+                    operatorArray[j] = string.Empty;
+                    while (operators.Contains(input[i]))
+                    {
+                        operatorArray[j] += input[i];
+                        i++;
+                    }
+                    i--;
+                    if (operatorArray[j].Length > 2)
+                        operatorArray[j] = operatorArray[j].Remove(0, operatorArray[j].Length - 2);
+                }
+                else
+                {
+                    if (points.Contains(input[i]))
+                    {
+                        if (numberString.Contains(input[i]) is false)
+                            numberString += input[i];
+                    }
+                    else numberString += input[i];
+                }
+            }
+
+
+
+
+
+            Console.WriteLine(numberString);
+            for (int i = 0; i < operatorArray.Length; i++)
+            {
+                Console.WriteLine(operatorArray[i]);
             }
         }
     }
